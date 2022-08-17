87c3676f0351eb26b60ddc565d8cf8a60ff1fc95
excellence-arch <oladipupomichael9@gmail.com>
2022-08-17 10:40:49 +0100

    starting the login page

--- src/Pages/Register.js
+++ src/Pages/Register.js
@@ -2,7 +2,13 @@ import React from 'react'
 
 const Register = () => {
   return (
-    <div>Register</div>
+    <div className='colors w-100 h-100 container-fluid'>
+        <div className='row'>
+            <form className='col-12 col-lg-7 mx-auto mt-5'>
+                <p className='text-center display-4'>Register</p>
+            </form>
+        </div>
+    </div>
   )
 }
 
