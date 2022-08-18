fab18bcff0942f55d8bb2468e88a30abc1c4fcef
excellence-arch <oladipupomichael9@gmail.com>
2022-08-18 14:39:39 +0100

    disabled a linting rule

--- src/Components/LatestTransactions.js
+++ src/Components/LatestTransactions.js
@@ -1,7 +1,7 @@
+/* eslint-disable react/prop-types */
 import React from 'react';
 import MaterialIcon from 'react-google-material-icons';
 
-// eslint-disable-next-line react/prop-types, no-unused-vars
 const LatestTransactions = ({ data }) => {
   return (
     <div className="mx-auto col-12 col-lg-6">
@@ -13,7 +13,7 @@ const LatestTransactions = ({ data }) => {
         </p>
         <div>
           <table className="table table-bordered mt-5">
-            {/* {data.map((val, ind) => (
+            {data.map((val, ind) => (
               <tr key={ind} className="p-4">
                 <td className="fw-bold p-3">{val.day}</td>
                 <td className="text-secondary">{val.acc_no}</td>
@@ -21,7 +21,7 @@ const LatestTransactions = ({ data }) => {
                 <td className="">{val.name}</td>
                 <td className="fw-bold">{val.amount}</td>
               </tr>
-            ))} */}
+            ))}
             <tr>
               <td colSpan={5} className="fw-bold text-center p-3">
                 See more{' '}
