05f3d9e3d84274cebb37f02ab92e78d1af1d0e9d
excellence-arch <oladipupomichael9@gmail.com>
2022-09-10 20:08:00 +0100

    donor dashboard

--- src/Components/Card.js
+++ src/Components/Card.js
@@ -5,7 +5,7 @@ const Card = ({ name, img, i }) => {
   return (
     <div key={i} className="borders mx-2 my-card">
       <img src={img} width="71px" className="rounds-start rounds-end text-center m-3" />
-      <p className="px-3 ms-2">{name}</p>
+      <p className="px-3 ms-2 text-muted">{name}</p>
     </div>
   );
 };
