402013abb0967170b3e4d88046ae01041323253f
excellence-arch <oladipupomichael9@gmail.com>
2022-09-14 22:25:19 +0100

    to add clippy to the success page

--- src/Pages/Success.js
+++ src/Pages/Success.js
@@ -1,20 +1,24 @@
 import React from 'react';
 // import success from '../assets/success1.gif';
 import success2 from '../assets/success2.gif';
-import NewNav from '../Layouts/NewNav';
+// import NewNav from '../Layouts/NewNav';
 
 const Success = () => {
   return (
-    <div className="">
-      <NewNav />
+    <div className="colorss h-100 p-5">
+      {/* <NewNav /> */}
       <div className="text-center">
-        <img src={success2} className="bg-image" width={'400px'} />
-        <p className="text-muted fw-bold fs-3" style={{ marginTop: '-50px' }}>
-          Donation sent
-        </p>
-        <p className="text-muted">
-          You&rsquo;ll recieve an Nft badge as an expression of our gratitude for this donation
-        </p>
+        <div
+          className="bg-white p-4 rounds-start rounds-end"
+          style={{ width: 'fit-content', marginLeft: '380px' }}>
+          <img src={success2} className="bg-image" width={'400px'} />
+          <p className="text-muted fw-bold fs-3" style={{ marginTop: '-50px' }}>
+            Donation sent
+          </p>
+          <p className="text-muted">
+            You&rsquo;ll recieve an Nft badge as an expression of our gratitude for this donation
+          </p>
+        </div>
       </div>
     </div>
   );
