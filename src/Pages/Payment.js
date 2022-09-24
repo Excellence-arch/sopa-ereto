412aeb31e15ef6e1dfc245c38440d8f3d961af8a
excellence-arch <oladipupomichael9@gmail.com>
2022-09-24 21:27:52 +0300

    about to integrate crypto payment

--- src/Pages/Payment.js
+++ src/Pages/Payment.js
@@ -4,7 +4,7 @@ import { useSelector } from 'react-redux';
 import { useNavigate } from 'react-router';
 // import $ from 'jquery';
 // import card from '../assets/card.png';
-// import circle from '../assets/circle.png';
+import circle from '../assets/circle.png';
 import cards from '../assets/Rectangle.png';
 import CryptoModal from '../Components/CryptoModal';
 import Modal from '../Components/Modal';
@@ -13,8 +13,8 @@ import NewNav from '../Layouts/NewNav';
 const Payment = () => {
   const [show, setShow] = useState(false);
   const [cryptoShow, setCryptoShow] = useState(false);
-  const [amount, setAmount] = useState('10');
-  const [currency, setCurrency] = useState('USD');
+  const [amount, setAmount] = useState('1');
+  const [currency, setCurrency] = useState();
   const [isloading, setIsloading] = useState(false);
   // const [isLoadingCrypto, setIsLoadingCrypto] = useState(false);
   const [cardNo, setCardNo] = useState();
@@ -22,12 +22,24 @@ const Payment = () => {
   const [expDate, setExpDate] = useState();
   const [keys, setKeys] = useState();
   const [newError, setNewError] = useState();
+  const [loads, setLoads] = useState(false);
   const [error, setError] = useState(false);
   const baseUrl = `${useSelector((state) => state.urlReducer.payments)}`;
   const navigate = useNavigate();
   const userDets = useSelector((state) => state.onlineReducer);
   const [paymentMethod, setPaymentMethod] = useState('card');
   const [walletAddress, setWalletAddress] = useState();
+  const allCurrencies = {
+    BTC: 'Bitcoin',
+    ETH: 'Ethreum',
+    ALGO: 'Algorand',
+    FLOW: 'Flow',
+    HBAR: 'Hedera',
+    MATIC: 'Polygon',
+    SOL: 'Solana',
+    TRX: 'Tron',
+    XLM: 'Stellar'
+  };
   const handleClose = () => setShow(false);
   const handleShow = () => setShow(true);
   const getKeys = () => {
@@ -35,9 +47,11 @@ const Payment = () => {
       setError('All fields arre required');
     } else {
       const newUrl = `${baseUrl}/mcs3/key`;
+      setLoads(true);
       axios
         .get(newUrl)
         .then((res) => {
+          setLoads(false);
           if (paymentMethod == 'card') {
             setKeys(res.data);
             setShow(true);
@@ -77,7 +91,24 @@ const Payment = () => {
   };
 
   const payCrypto = () => {
-    console.log('I want to pay with crypto');
+    setIsloading(true);
+    axios
+      .post(`${baseUrl}/mcs3/createCard`, {
+        id: keys.keyId,
+        publicKey: keys.publicKey,
+        amount,
+        walletAddress: walletAddress.trim()
+      })
+      .then((res) => {
+        setIsloading(false);
+        if (res.data.data.status == 'pending') {
+          navigate('/donor/success');
+        }
+      })
+      .catch((err) => {
+        setIsloading(false);
+        setNewError(err.message);
+      });
   };
 
   return (
@@ -92,7 +123,30 @@ const Payment = () => {
           <p className="fw-bold mb-0" style={{ color: 'rgb(210,105,30)', fontSize: '30px' }}>
             Your donation will save the Massai!
           </p>
-          <p className="fonts mb-5">Enter your payment info below</p>
+          <p className="fonts mb-3">Enter your payment info below</p>
+          <div className="mb-3">
+            <input
+              type={'radio'}
+              className="radios"
+              onClick={(e) => setPaymentMethod(e.target.value)}
+              name="payment"
+              defaultChecked
+              value={'card'}
+            />
+            <span className="fonts text-muted">&nbsp;Donate with Card</span>
+            {/* </span> */}
+            &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;
+            {/* <span> */}
+            <input
+              type={'radio'}
+              name="payment"
+              onClick={(e) => setPaymentMethod(e.target.value)}
+              value={'crypto'}
+              className="radios"
+            />
+            <span className="fonts text-muted">&nbsp;Donate with Crypto</span>
+          </div>
+
           <label className="fonts text-blue">Email Address</label>
           <input
             type="text"
@@ -108,50 +162,55 @@ const Payment = () => {
             onChange={(e) => setAmount(e.target.value)}
             className="form-control w-50 mb-3 border-0 check-weight py-2"
           />
-          <label className="fonts text-blue">Currency</label>
-          <br />
-          <select
-            className="text-danger checks months fw-light p-2 mb-3 rounded"
-            onChange={(e) => setCurrency(e.target.value)}>
-            <option value="USD">USD</option>
-            <option value="KES">KES</option>
-            <option value="GBP">GBP</option>
-          </select>
-          <br />
-          <label className="fonts text-blue">Country</label>
-          <br />
-          <select className="text-danger checks months fw-light p-2 rounded mb-3">
-            <option>US</option>
-            <option>Kenya</option>
-            <option>Great Britain</option>
-          </select>
-          <br />
-          {/* <span> */}
-          <input
-            type={'radio'}
-            className="radios"
-            onChange={(e) => setPaymentMethod(e.target.value)}
-            name="payment"
-            checked
-            value={'card'}
-          />
-          <span className="fonts text-muted">&nbsp;Donate with Card</span>
-          {/* </span> */}
-          &nbsp;&nbsp; &nbsp;&nbsp; &nbsp;
-          {/* <span> */}
-          <input
-            type={'radio'}
-            name="payment"
-            onChange={(e) => setPaymentMethod(e.target.value)}
-            value={'crypto'}
-            className="radios"
-          />
-          <span className="fonts text-muted">&nbsp;Donate with Crypto</span>
-          {/* </span> */}
-          <br />
-          <button className="btn btn-color px-4 mt-4" onClick={getKeys}>
-            Donate
-          </button>
+          {paymentMethod == 'card' ? (
+            <div>
+              <label className="fonts text-blue">Currency</label>
+              <br />
+              <select
+                className="text-danger checks months fw-light p-2 mb-3 rounded"
+                onChange={(e) => setCurrency(e.target.value)}>
+                <option value="USD">USD</option>
+                <option value="KES">KES</option>
+                <option value="GBP">GBP</option>
+              </select>
+              <br />
+              <label className="fonts text-blue">Country</label>
+              <br />
+              <select className="text-danger checks months fw-light p-2 rounded mb-3">
+                <option>US</option>
+                <option>Kenya</option>
+                <option>Great Britain</option>
+              </select>
+            </div>
+          ) : (
+            <div>
+              <label className="fonts text-blue">Currency</label>
+              <br />
+              <select
+                className="text-danger checks months fw-light p-2 mb-3 rounded"
+                onChange={(e) => setCurrency(e.target.value)}>
+                <option value="BTC">BTC</option>
+                <option value="ETH">ETH</option>
+                <option value="ALGO">ALGO</option>
+                <option value="FLOW">FLOW</option>
+                <option value="HBAR">HBAR</option>
+                <option value="MATIC">MATIC</option>
+                <option value="SOL">SOL</option>
+                <option value="TRX">TRX</option>
+                <option value="XLM">XLM</option>
+              </select>
+              <span>&nbsp;&nbsp;{allCurrencies[currency]}</span>
+            </div>
+          )}
+
+          <div>
+            <button className="btn btn-color px-4 mt-4 " onClick={getKeys}>
+              {loads == true ? 'Please Wait...' : 'Donate'}
+            </button>
+            <p style={{ marginTop: '-40px', marginLeft: '90px' }}>
+              Powered by <img src={circle} alt="circle.com logo" width={'90px'} />
+            </p>
+          </div>
         </div>
       </div>
       <Modal
--- src/Reducers/online.js
+++ src/Reducers/online.js
@@ -1,5 +1,5 @@
 const initState = {
-  status: false,
+  status: true,
   id: '',
   email: '',
   typeOf: '',
